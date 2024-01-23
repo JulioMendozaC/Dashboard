580fd8cec4ed957ee1117f8e490396f5c3cca157
JulioMendozaC <julio.mendozc@gmail.com>
2024-01-22 22:32:19 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import {useState} from "react";
+// import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/Global/TopBar";
+import Sidebar from "./scenes/global/SideBar";
+// import Dashboard from "./scenes/dashboard";
+// import Team from "./scenes/team";
+// import Invoices from "./scenes/invoices";
+// import Contacts from "./scenes/contacts";
+// import Bar from "./scenes/bar";
+// import Form from "./scenes/form";
+// import Line from "./scenes/line";
+// import Pie from "./scenes/pie";
+// import FAQ from "./scenes/faq";
+import {CssBaseline, ThemeProvider} from "@mui/material";
+import {ColorModeContext, useMode} from "./thems";
+// import Calendar from "./scenes/calendar/calendar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
